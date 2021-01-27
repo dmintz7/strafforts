ce4ac52317b9402367d0b02928ad11b52097352d
dmintz7 <dmintz37@gmail.com>
2021-01-27 14:26:35 -0500

    Update development.rb
    smtp wasnt working
    sendmail doesnt give error

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.smtp_settings = {
       address:              ENV['SMTP_PROVIDER_ADDRESS'],
       port:                 ENV['SMTP_PROVIDER_PORT'],
